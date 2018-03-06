7cac79f6576ad2de80c67b9d2a21820e40ea228f
ninho <ivanilsonjunior@gmail.com>
2018-03-06 09:01:46 -0300

    Detalhamento de cada ação

--- LeituraDePaginas/versaoKernel.py
+++ LeituraDePaginas/versaoKernel.py
@@ -1,6 +1,10 @@
+# Nesse exemplo a busca do lxml eh feita em um arquivo XML
 import requests
 from lxml import html
+
 pagina = requests.get("https://www.kernel.org/feeds/kdist.xml")
 conteudo = html.fromstring(pagina.content)
-versao = conteudo.xpath('//item/title/text()')[0] #Pegando o primeiro elemento da lista
+
+# Fazendo a busca pelos title dos itens, ele retorna uma lista, estamos recuperando o primeiro elemento da lista [0] e jogando na tela
+versao = conteudo.xpath('//item/title/text()')[0]  # Pegando o primeiro elemento da lista
 print (versao)
