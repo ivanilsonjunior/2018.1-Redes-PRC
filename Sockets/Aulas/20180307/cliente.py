3891e13e9488c566e0aa869fe2da4519a4fb01ed
ninho <ivanilsonjunior@gmail.com>
2018-03-07 20:09:13 -0300

    Endereço ip correto

--- Sockets/Aulas/20180307/cliente.py
+++ Sockets/Aulas/20180307/cliente.py
@@ -1,10 +1,13 @@
 # Echo client program
 import socket
 
-HOST = '10.27.2.214'    # The remote host
+HOST = '10.24.4.102'    # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Nick: Mensagem')
+    s.sendall(b'Ninho: Teste de envio')
     data = s.recv(1024)
+    teste = [data.decode('utf-8') for data in teste]
 print('Recebido: ', repr(data))
+for i in teste:
+    print (i)
