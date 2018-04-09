7b490566096e9c0fb2de8886207b6770517d65d6
Conta Aluno - IFRN <ivanilsonjunior@gmail.com>
2018-04-09 04:57:26 -0700

    new file:   BancoDeDados/PythonDBLinux.sh

---
+++ BancoDeDados/PythonDBLinux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Instalacao de Bibliotecas basicas no linux
+#
+export PATH=/usr/lib/postgresql/9.5/bin/:$PATH
+echo "Lembre-se que a senha de aluno é aluno"
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install psycopg2 
+sudo -H pip3 install psycopg2-binary
