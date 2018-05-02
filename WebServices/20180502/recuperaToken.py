6d679481fe79fc81131a64058539e3c8fc9ea20c
Ninho <ivanilsonjunior@gmail.com>
2018-05-02 18:15:53 -0300

    Deixando o código mais elegante

--- WebServices/20180502/recuperaToken.py
+++ WebServices/20180502/recuperaToken.py
@@ -1,8 +1,8 @@
 import json
 import requests
 
-urls = dict([('token', 'https://suap.ifrn.edu.br/api/v2/autenticacao/token/'),
-             ('dados', 'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/')])
+urls = { "token":"https://suap.ifrn.edu.br/api/v2/autenticacao/token/",
+         "dados":"https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/"}
 
 autenticacao = {
     "username": "SUA_MATRICULA",
@@ -19,7 +19,6 @@ def getToken():
 
 cabecalho={'Authorization': 'JWT {0}'.format(getToken())}
 
-
 def getInformacoes():
     response = requests.get(urls['dados'], headers=cabecalho)
     if response.status_code == 200:
@@ -28,5 +27,7 @@ def getInformacoes():
         return None
 
 informacoes = json.loads(getInformacoes())
-print ("Matricula:", informacoes['matricula'], "\n\tNome:", informacoes['nome_usual'], "\n\tE-Mail:", informacoes['email'])
-
+# Para ver o conteudo de informacoes, descomente a linha abaixo
+# VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+#print (informacoes)
+print ("Matricula: {0}\n\tNome: {1}\n\tE-Mail: {2}".format(informacoes['matricula'], informacoes['nome_usual'], informacoes['email']))
